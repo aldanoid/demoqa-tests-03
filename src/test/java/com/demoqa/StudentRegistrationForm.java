32192cbc4e8701243d267f315827049a68858a68
Roman Bogdanov <aldanstream@gmail.com>
2023-07-31 05:13:41 -0300

    added Student Registration Form 02

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationForm {
