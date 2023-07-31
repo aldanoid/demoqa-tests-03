18757ed240408f2f917bbdbdd0bac5f3a8a3768b
Roman Bogdanov <aldanstream@gmail.com>
2023-07-31 04:51:56 -0300

    added Student Registration Form 02

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -24,12 +24,20 @@ public class StudentRegistrationForm {
         $("#firstName").setValue("Ivan");
         $("#lastName").setValue("Drago");
         $("#userEmail").setValue("ivan@drago.com");
-        $("#genterWrapper").setValue("#gender-radio-1");
+        $("#genterWrapper").setValue("Male").pressEnter();
         $("#userNumber").setValue("1113212155");
-        $("#dateOfBirthInput").setValue("01 Jan 2000");
-        $("#subjectsContainer").setValue("");
-        $("#hobbiesWrapper").setValue("#hobbies-checkbox-3");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__day.react-datepicker__day--001").click();
+
+        $("#subjectsContainer").setValue("Physics").pressEnter();
+
+        $("#hobbiesWrapper#hobbies-checkbox-3").click();
+
         $("#uploadPicture").setValue("");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/photo.jpeg"));
         $("#currentAddress").setValue("Rio 1135");
         $("#state").setValue("Rajasthan");
         $("#city").setValue("Jaipur");
